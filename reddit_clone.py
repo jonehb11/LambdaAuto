8af254701f2a02a6e44e455ad749fbe6b37e7356
jonehb11 <93842893+jonehb11@users.noreply.github.com>
2023-09-26 14:01:58 -0600

    Create reddit_clone.py
    reddit clone with authentication/ensure you install uvicorn

---
+++ reddit_clone.py
@@ -0,0 +1,137 @@
+from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from typing import List, Optional
+#install uvicron via 'pip install uvicorn[all]
+app = FastAPI()
+
+# Configuration settings for JWT
+SECRET_KEY = "your-secret-key"  # Replace with a secure  key
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+# Mock database (you should replace this with a real database)
+posts_db = []
+users_db = []
+
+class Post(BaseModel):
+    title: str
+    content: str
+    owner: str
+
+class UserBase(BaseModel):
+    username: str
+
+class UserCreate(UserBase):
+    password: str
+
+class User(UserBase):
+    id: int
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+class UserInDB(User):
+    hashed_password: str
+
+# Security
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_user(username: str):
+    for user in users_db:
+        if user["username"] == username:
+            return User(**user)
+
+# Create a function to authenticate a user and create an access token
+def authenticate_user(username: str, password: str):
+    user = get_user(username)
+    if not user or not verify_password(password, user.hashed_password):
+        return None
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    return token_data
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@app.post("/users/", response_model=User)
+async def create_user(user: UserCreate):
+    hashed_password = pwd_context.hash(user.password)
+    new_user = UserInDB(**user.dict(), hashed_password=hashed_password, id=len(users_db) + 1)
+    users_db.append(new_user.dict())
+    return new_user
+
+@app.get("/users/{username}", response_model=User)
+async def read_user(username: str):
+    user = get_user(username)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+@app.get("/users/", response_model=List[User])
+async def read_users(skip: int = 0, limit: int = 10):
+    return users_db[skip : skip + limit]
+
+@app.post("/posts/", response_model=Post)
+async def create_post(post: Post, current_user: TokenData = Depends(get_current_user)):
+    post_dict = post.dict()
+    post_dict["owner"] = current_user.username
+    posts_db.append(post_dict)
+    return post
+
+@app.get("/posts/", response_model=List[Post])
+async def get_posts(skip: int = 0, limit: int = 10):
+    return posts_db[skip : skip + limit]
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
